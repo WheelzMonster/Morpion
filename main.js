b4484b4dcfa1bee8d6a6dc67997941ef024d3b10
Nicolas Lafitte <nicolaslafitte@users.noreply.github.com>
2018-10-13 11:28:14 +0200

    fix: end of the game

--- main.js
+++ main.js
@@ -1,48 +1,85 @@
-var pairimpaire = 0;
+var turnCount = 0;
+var cases = document.querySelectorAll('.case');
+var oneWinner = false;
 
-var cases = document.querySelectorAll(".case");
 function turn() {
-    pairimpaire++;
-    return pairimpaire % 2 === 0 ? "X" : "O";
+  turnCount++;
+  return turnCount % 2 === 0 ? 'X' : 'O';
+}
+
+function reset() {
+  cases.forEach(vide => (vide.innerHTML = ''));
+  oneWinner = false;
+  turnCount = 0;
 }
 
 function showWinner(winner) {
-    alert(`${winner} est le vainqueur! Rejouez!`)
-    cases.forEach(vide => vide.innerHTML = '');
+  oneWinner = true;
+  alert(`${winner} est le vainqueur! Rejouez!`);
+  reset();
 }
 
-function winState() {
-    if (cases[0].innerHTML !== '' && cases[0].innerHTML === cases[1].innerHTML && cases[1].innerHTML === cases[2].innerHTML) {
-       showWinner(cases[0].innerHTML); 
-    }
-    else if (cases[3].innerHTML !== '' && cases[3].innerHTML === cases[4].innerHTML && cases[4].innerHTML === cases[5].innerHTML) {
-        showWinner(cases[3].innerHTML);
-    }
-    else if (cases[6].innerHTML !== '' && cases[6].innerHTML === cases[7].innerHTML && cases[7].innerHTML === cases[8].innerHTML) {
-        showWinner(cases[6].innerHTML);
-    }
-    else if (cases[0].innerHTML !== '' && cases[0].innerHTML === cases[3].innerHTML && cases[3].innerHTML === cases[6].innerHTML) {
-        showWinner(cases[0].innerHTML);
-    }
-    else if (cases[1].innerHTML !== '' && cases[1].innerHTML === cases[4].innerHTML && cases[4].innerHTML === cases[7].innerHTML) {
-        showWinner(cases[1].innerHTML);
-    }
-    else if (cases[2].innerHTML !== '' && cases[2].innerHTML === cases[5].innerHTML && cases[5].innerHTML === cases[8].innerHTML) {
-        showWinner(cases[2].innerHTML);
-    } 
-    else if (cases[0].innerHTML !== '' && cases[0].innerHTML === cases[4].innerHTML && cases[4].innerHTML === cases[8].innerHTML) {
-        showWinner(cases[0].innerHTML);
-    } 
-    else if (cases[2].innerHTML !== '' && cases[2].innerHTML === cases[4].innerHTML && cases[4].innerHTML === cases[6].innerHTML) {
-        showWinner(cases[2].innerHTML);
-    }
+function checkWinState() {
+  if (
+    cases[0].innerHTML !== '' &&
+    cases[0].innerHTML === cases[1].innerHTML &&
+    cases[1].innerHTML === cases[2].innerHTML
+  ) {
+    showWinner(cases[0].innerHTML);
+  } else if (
+    cases[3].innerHTML !== '' &&
+    cases[3].innerHTML === cases[4].innerHTML &&
+    cases[4].innerHTML === cases[5].innerHTML
+  ) {
+    showWinner(cases[3].innerHTML);
+  } else if (
+    cases[6].innerHTML !== '' &&
+    cases[6].innerHTML === cases[7].innerHTML &&
+    cases[7].innerHTML === cases[8].innerHTML
+  ) {
+    showWinner(cases[6].innerHTML);
+  } else if (
+    cases[0].innerHTML !== '' &&
+    cases[0].innerHTML === cases[3].innerHTML &&
+    cases[3].innerHTML === cases[6].innerHTML
+  ) {
+    showWinner(cases[0].innerHTML);
+  } else if (
+    cases[1].innerHTML !== '' &&
+    cases[1].innerHTML === cases[4].innerHTML &&
+    cases[4].innerHTML === cases[7].innerHTML
+  ) {
+    showWinner(cases[1].innerHTML);
+  } else if (
+    cases[2].innerHTML !== '' &&
+    cases[2].innerHTML === cases[5].innerHTML &&
+    cases[5].innerHTML === cases[8].innerHTML
+  ) {
+    showWinner(cases[2].innerHTML);
+  } else if (
+    cases[0].innerHTML !== '' &&
+    cases[0].innerHTML === cases[4].innerHTML &&
+    cases[4].innerHTML === cases[8].innerHTML
+  ) {
+    showWinner(cases[0].innerHTML);
+  } else if (
+    cases[2].innerHTML !== '' &&
+    cases[2].innerHTML === cases[4].innerHTML &&
+    cases[4].innerHTML === cases[6].innerHTML
+  ) {
+    showWinner(cases[2].innerHTML);
+  }
+  if (turnCount == 9 && !oneWinner) {
+    alert('Pas de gagnant');
+    reset();
+  }
 }
 
 function game() {
-    if (!this.innerHTML) {
-        this.innerHTML = turn()
-        winState();
-    }
+  if (!this.innerHTML) {
+    this.innerHTML = turn();
+    checkWinState();
+  }
 }
 
-cases.forEach(caseSeule => caseSeule.addEventListener("click", game));
+cases.forEach(caseSeule => caseSeule.addEventListener('click', game));
